6466e9574773d416368f0d90eb3126e1559eda82
Jeeva7623 <jeevad807@gmail.com>
2023-08-31 17:55:37 +0530

    5th problem doing

---
+++ Javascript/demo.js
@@ -0,0 +1,32 @@
+a = [
+    {
+        'name':'Raddish',
+        'category':'vegetable',
+    },
+    {
+        'name':'Carrot',
+        'category':'vegetable',
+    },
+    {
+        'name':'Sweet potato',
+        'category':'Vegetable',
+    },
+    {
+        'name':'Dragon Fruit',
+        'category':'Fruit',
+    },
+    {
+        'name':'Apple',
+        'category':'Fruit',
+    },
+    {
+        'name':'Cheetah',
+        'category':'Animal',
+    },
+]
+cap = a.map(e => {
+    x={ };
+    x[e.category] = e.name;
+    return x;
+})
+console.log(cap)
