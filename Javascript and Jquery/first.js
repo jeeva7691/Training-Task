c8d16aa3c206b8e88c7485a63d0725f6b6a4b96a
Jeeva7623 <jeevad807@gmail.com>
2023-09-21 16:27:54 +0530

    Javacript

---
+++ Javascript and Jquery/first.js
@@ -0,0 +1,19 @@
+// alert("Hello_world")
+// function fun1(thisdate)
+// {
+//     document.write("Hello_world" + thisdate)
+// }
+
+// document.getElementById("btn").onClick = 
+// function()
+// {
+//     alert("Hi Iam you !")
+// };
+
+window.onload = function()
+{
+    document.getElementById("btn").onclick = 
+    function() {
+        alert("Hello_World Iam you!")
+    };
+};
