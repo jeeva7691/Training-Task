28a7c6051674946d5ed34ee5147195b43b44d5bb
Jeeva7623 <jeevad807@gmail.com>
2023-09-25 11:17:57 +0530

    portfolio and calculator

--- Javascript and Jquery/first.js
+++ Javascript and Jquery/first.js
@@ -1,8 +1,32 @@
 // alert("Hello_world")
-// function fun1(thisdate)
-// {
-//     document.write("Hello_world" + thisdate)
-// }
+function fun1(thisdate)
+{
+    function currentTime() {
+        let date = new Date(); 
+        let hh = date.getHours();
+        let mm = date.getMinutes();
+        let ss = date.getSeconds();
+        let session = "AM";
+      
+        // if(hh == 0){
+        //     hh = 12;
+        // }
+        // if(hh > 12){
+        //     hh = hh - 12;
+        //     session = "PM";
+        //  }
+      
+        //  hh = (hh < 10) ? "0" + hh : hh;
+        //  mm = (mm < 10) ? "0" + mm : mm;
+        //  ss = (ss < 10) ? "0" + ss : ss;
+          
+         let time = hh + ":" + mm + ":" + ss + " " + session;
+      
+        document.getElementById("btn").onclick = time; 
+        let t = setTimeout(function(){ currentTime() }, 1000);
+      }
+      currentTime();
+}
 
 // document.getElementById("btn").onClick = 
 // function()
@@ -10,10 +34,10 @@
 //     alert("Hi Iam you !")
 // };
 
-window.onload = function()
-{
-    document.getElementById("btn").onclick = 
-    function() {
-        alert("Hello_World Iam you!")
-    };
-};
+// window.onload = function()
+// {
+//     document.getElementById("btn").onclick = 
+//     function() {
+//         alert("Hello_World Iam you!")
+//     };
+// };
