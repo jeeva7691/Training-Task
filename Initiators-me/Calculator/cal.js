c61f8d9d2bda10cf48faf5a643a715ccd43ac129
Jeeva7623 <jeevad807@gmail.com>
2023-09-27 17:47:17 +0530

    user-journey-flow

--- Initiators-me/Calculator/cal.js
+++ Initiators-me/Calculator/cal.js
@@ -1,11 +1,38 @@
 function printText() {
-    // Get a reference to the textbox
     var textbox = document.getElementById("out");
-
-    // Set the text in the textbox
     textbox.value = "1";
 }
-
-// Attach the click event handler to the button
 var button = document.getElementById("one");
 button.addEventListener("click", printText);
+function printText1() {
+    var textbox = document.getElementById("out");
+    textbox.value="";
+}
+var button = document.getElementById("AC");
+button.addEventListener("click", printText1);
+function printText2() {
+    var textbox = document.getElementById("out");
+    textbox.value="2";
+}
+var button = document.getElementById("Two");
+button.addEventListener("click", printText2);
+
+function getInputValue() {
+    let inputVal = document.getElementById("out").value;
+    var n1 = Number(inputVal);
+    console.log(n1);
+    // document.getElementById("out").value  = n1 + n2;
+    // console.log(n1 + n2);
+    // out.value="";  
+ }
+
+
+function plus(){
+    
+    let inputVal2 = document.getElementById("out").value;
+    var n2 = Number(inputVal2);
+    console.log(n2);
+    // let add = inputval + inputVal;
+    // alert(add);
+
+}
